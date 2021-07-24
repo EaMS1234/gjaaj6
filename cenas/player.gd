5d7e2cfac44d8c8ae1f1c8c395dd5740d9f0866b
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2021-07-24 16:31:04 -0300

    Movimento básico do jogador finalizado

---
+++ cenas/player.gd
@@ -0,0 +1,30 @@
+# Movimento do jogador
+
+extends Area2D
+
+export var velo = 150  # Velocidade
+
+var mov = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_down"):
+		mov.y += velo
+	
+	if Input.is_action_pressed("ui_up"):
+		mov.y -= velo
+	
+	if Input.is_action_pressed("ui_left"):
+		mov.x -= velo
+	
+	if Input.is_action_pressed("ui_right"):
+		mov.x += velo
+	
+	if Input.is_action_pressed("ui_accept"):
+		pass
+	
+	if mov.length() >= velo:
+		# Impede de se mover mais rapido que a velocidade estabelecida
+		mov = mov.normalized() * velo
+	
+	self.position += mov * delta
+	mov = mov * 0  # Jogador para quando nao esta recebendo nenhuma força
