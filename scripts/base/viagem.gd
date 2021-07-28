5e889627fcdc1118fdf9503fc24fa92fd0791a3f
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2021-07-27 21:36:34 -0300

    Menu viagem adicionado

---
+++ scripts/base/viagem.gd
@@ -0,0 +1,42 @@
+extends Polygon2D
+
+signal pa
+signal pre
+signal fu
+signal atual
+
+var cursor = 1
+
+func _ready():
+	for node in get_tree().get_nodes_in_group("__player"):
+		node.movable = false
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("viagem"):
+		for node in get_tree().get_nodes_in_group("__player"):
+			node.movable = true
+		
+		queue_free()
+	
+	elif Input.is_action_just_pressed("ui_left"):
+		if cursor > 0:
+			cursor -= 1
+		
+	elif Input.is_action_just_pressed("ui_right"):
+		if cursor < 2:
+			cursor += 1
+	
+	if cursor == 0:
+		$pa.offset.y = -25
+		$pre.offset.y = 0
+		$fu.offset.y = 0
+	
+	elif cursor == 1:
+		$pa.offset.y = 0
+		$pre.offset.y = -25
+		$fu.offset.y = 0
+	
+	elif cursor == 2:
+		$pa.offset.y = 0
+		$pre.offset.y = 0
+		$fu.offset.y = -25
