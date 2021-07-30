453c82c525662d1756dd57a5d0210002c674940a
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2021-07-29 22:32:59 -0300

    Sprite de player carregando caixa agora atualiza corretamente

--- cenas/base/branco.gd
+++ cenas/base/branco.gd
@@ -4,6 +4,7 @@ var x = float(0)
 var etapa = 0
 
 var alvo = 1
+var rep = 0
 
 func _ready():
 	$AudioStreamPlayer2D.play()
@@ -15,6 +16,14 @@ func _physics_process(delta):
 		x += 0.000025
 		
 		if x >= 0.005:
+			if alvo != 1 and rep == 0 and get_tree().get_nodes_in_group("__player")[0].pegou == true:
+				get_tree().get_nodes_in_group("__player")[0].get_child(1).animation = "c_frente_p"
+				rep += 1
+			
+			elif alvo == 1 and rep == 0 and get_tree().get_nodes_in_group("__player")[0].pegou == true:
+				get_tree().get_nodes_in_group("__player")[0].get_child(1).animation = "c_frente"
+				rep += 1
+			
 			for node in get_tree().get_nodes_in_group("__sala"):
 				node.tempo = alvo
 			
